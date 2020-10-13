24418b3baca1a1869429bbf84a3d9672f7401d22
chuyue <chuyue.liu@gmail.com>
2020-10-13 22:16:23 +0800

    aaa

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject followTarget;
+	private Vector3 targetPos;
+	public float moveSpeed;
+    private static bool cameraExists;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!cameraExists)
+        {
+            cameraExists = true;
+            DontDestroyOnLoad(gameObject.transform);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+		targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y + 2, transform.position.z);
+		transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -35,7 +35,6 @@ public class PlayerController : MonoBehaviour
 
 		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
 		{
-			//transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime,0f,0f));
 			myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
 			playerMoving = true;
 			lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
@@ -46,7 +45,6 @@ public class PlayerController : MonoBehaviour
 		}
 		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
 		{
-			//transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
 			myRigidbody.velocity = new Vector2(myRigidbody.velocity.x,Input.GetAxisRaw("Vertical") * moveSpeed);
 			playerMoving = true;
 			lastMove = new Vector2(0,Input.GetAxisRaw("Vertical"));
